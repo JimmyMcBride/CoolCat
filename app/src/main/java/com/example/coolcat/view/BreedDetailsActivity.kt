b8cf0cda6709b6b50ee2698f3bae9d3040a74695
Jimmy McBride <mcbride967@gmail.com>
2021-11-04 12:39:18 -0400

    update

---
+++ app/src/main/java/com/example/coolcat/view/BreedDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coolcat.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BreedDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breed_details)
+    }
+}
